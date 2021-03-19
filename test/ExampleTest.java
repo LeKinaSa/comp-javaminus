2cb550caa74590d5e6ce7096126e66e177951c74
Gonçalo Pascoal <up201806332@fe.up.pt>
2021-03-19 16:28:39 +0000

    Added more tests

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -27,4 +27,46 @@ public class ExampleTest {
         String jmmCode = SpecsIo.getResource("fixtures/public/QuickSort.jmm");
         TestUtils.noErrors(TestUtils.parse(jmmCode).getReports());
     }
+
+    @Test
+    public void testLazySort() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/LazySort.jmm");
+        TestUtils.noErrors(TestUtils.parse(jmmCode).getReports());
+    }
+
+    @Test
+    public void testFindMaximum() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/FindMaximum.jmm");
+        TestUtils.noErrors(TestUtils.parse(jmmCode).getReports());
+    }
+
+    @Test
+    public void testLife() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/Life.jmm");
+        TestUtils.noErrors(TestUtils.parse(jmmCode).getReports());
+    }
+
+    @Test
+    public void testMonteCarloPi() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/MonteCarloPi.jmm");
+        TestUtils.noErrors(TestUtils.parse(jmmCode).getReports());
+    }
+
+    @Test
+    public void testTicTacToe() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/TicTacToe.jmm");
+        TestUtils.noErrors(TestUtils.parse(jmmCode).getReports());
+    }
+
+    @Test
+    public void testWhile() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/fail/syntactical/CompleteWhileTest.jmm");
+        TestUtils.noErrors(TestUtils.parse(jmmCode).getReports());
+    }
+
+    @Test
+    public void testJson() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/HelloWorld.jmm");
+        System.out.println(TestUtils.parse(jmmCode).toJson());
+    }
 }
